4cf87ad8977fe04858b8f65f632c8e13a2ec5779
Tomas Goncalves <43731728+Runner-dev@users.noreply.github.com>
2024-10-03 14:43:33 -0400

    improve scripts to be pi-agnostic

--- bin/from_pi.sh
+++ bin/from_pi.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
+
+# Check if hostname is provided as an argument
+if [ -z "$1" ]; then
+  echo "Usage: $0 <hostname>"
+  exit 1
+fi
+
+# Set the hostname from the first argument
+HOSTNAME=$1
+
 # Set the working directory to the location of the script
 cd "$(dirname "$0")/.."
 
 # Rsync command
 rsync -avz \
-  cmr@raspberrypi.local:~/24e-DAQ-v2-Website/rasppi/tests rasppi
+  cmr@"$HOSTNAME":~/24e-DAQ-v2-Website/rasppi/tests rasppi
--- bin/to_pi.sh
+++ bin/to_pi.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
+
+# Check if hostname is provided as an argument
+if [ -z "$1" ]; then
+  echo "Usage: $0 <hostname>"
+  exit 1
+fi
+
+# Set the hostname from the first argument
+HOSTNAME=$1
+
 # Set the working directory to the location of the script
 cd "$(dirname "$0")/.."
 
 # Your rsync command
-rsync -avz --exclude-from='.gitignore' --exclude 'node_modules' --exclude 'rasppi/tests' --exclude '.git' --exclude 'videos' . cmr@raspberrypi.local:~/24e-DAQ-v2-Website
+rsync -avz --exclude-from='.gitignore' --exclude 'node_modules' --exclude 'rasppi/tests' --exclude '.git' --exclude 'videos' . $HOSTNAME:~/24e-DAQ-v2-Website
