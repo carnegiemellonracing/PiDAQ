4f61f471024dc3b57705eb3aa664140d8169757b
Meme-Account <tanish.bengani@gmail.com>
2024-10-24 03:03:31 -0400

    removed sync from graphs

--- client/src/components/DataGraph.jsx
+++ client/src/components/DataGraph.jsx
@@ -12,7 +12,7 @@ export default function DataGraph({ data, testName }) {
     return (
         <>
             <ResponsiveContainer width="100%" height={100}>
-                <LineChart data={data} syncId={testName}>
+                <LineChart data={data}>
                     <CartesianGrid strokeDasharray="3 3" />
                     <XAxis dataKey="independent" />
                     <YAxis />
