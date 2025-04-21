bdac33539409d780ddc41fe11ee4d66ab3dcb516
Varun Rajesh <varun.rajesh81@gmail.com>
2025-04-20 21:18:16 -0400

    Removed unnecessary files

--- bin/fix_csv.py
+++
@@ -1,9 +0,0 @@
-import sys
-
-fileName = sys.argv[1]
-
-with open(fileName, 'r') as file:
-    with open('fixed_' + fileName, 'a') as fixed_file:
-        fixed_file.write(file.readline())
-        for line in file:
-            fixed_file.write(line.replace('[', '"[').replace(']', ']"'))
--- rasppi/README.md
+++
@@ -1,3 +0,0 @@
-# PiDAQ
-
-**PiDAQ**: A Raspberry **Pi**-based **Data Acquisition** system for Carnegie Mellon Racing. Designed for 24e and 25e EV racecars.
